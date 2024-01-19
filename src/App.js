4552381d5e864d62300778a992063b565b6ca42a
Асад <asilbekroziev@MacBook-Pro-Asad.local>
2024-01-19 22:32:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PostDetails from './Components/Posts - Details/Posts.Detaisl';
+import Header from './Components/Header/Header';
+import Posts from './Components/Posts/Posts';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+    <Router>
+      <Routes>
+        <Route path="/posts/:id" element={<PostDetails />} />
+        <Route path="/" element={<Posts />} />
+      </Routes>
+    </Router>
     </div>
   );
-}
+};
 
 export default App;
